e86f1e599bb37ffc66880b1d0e2e36608d308e42
vladislavikys <49569336+vladislavikys@users.noreply.github.com>
2022-06-30 14:43:32 +0300

    add print value dict

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,24 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let softTime = 5
-    let mediumTime  = 7
-    let hardTime = 12
+    let eggTimes = [ "Soft": 5 , "Medium": 7 ,"Hard": 12]
+    
     
 
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        let  hardness = sender.currentTitle
+        let  hardness = sender.currentTitle!
         
-        switch hardness {
-        case "Soft":
-            print(softTime)
-        case "Mediun":
-            print(mediumTime)
-        case "Hard":
-            print(hardTime)
-        default:
-            print("you lose ")
-        }
+        print(eggTimes[hardness]!)
+        
+        
+        
     }
     
    
