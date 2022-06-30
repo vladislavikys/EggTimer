de190aaac9a9021f788409c7c9df11eea13a2d6f
vladislavikys <49569336+vladislavikys@users.noreply.github.com>
2022-06-30 10:13:03 +0300

    add print sender.currentTitle button

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let softTime = 5
+    let mediumTime  = 7
+    let hardTime = 12
+    
 
 
     
@@ -17,5 +21,21 @@ class ViewController: UIViewController {
     
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
+        var hardness = sender.currentTitle
+        
+        if hardness == "Soft"{
+            print("Soft ")
+        }else if  hardness == "Medium"{
+            print("Medium")
+        }else if hardness == "Hard"{
+            print("Hard")
+        }
     }
+    
+   
+    
+     
+    
+    
+    
 }
