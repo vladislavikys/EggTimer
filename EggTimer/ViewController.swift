c770feda4acde68c36e04fa646ad75da9e414e01
vladislavikys <49569336+vladislavikys@users.noreply.github.com>
2022-06-30 20:46:58 +0300

    timer second work

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,18 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let eggTimes = [ "Soft": 300 , "Medium": 420 ,"Hard": 720]
-    var secondRemaining = 60
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    let eggTimes = [ "Soft": 3 , "Medium": 4 ,"Hard": 7]
+    var secondRemaining = 0
+    var timer = Timer()
    
     @IBAction func hardnessSelected(_ sender: UIButton) {
         let  hardness = sender.currentTitle!
+        timer.invalidate()
         
         //получаем силу варки яйца
         
         secondRemaining  =  eggTimes[hardness]!
         // на основе этого опбращаемся по ключу за время  готовки
         
-       Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+       timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
     }
     
     @objc func updateCounter() {
@@ -29,6 +34,9 @@ class ViewController: UIViewController {
        if secondRemaining > 0  {
            print("\(secondRemaining)")
            secondRemaining -= 1
+       }else if secondRemaining == 0 {
+           timer.invalidate()
+           titleLabel.text = "DONE!"
        }
    }
     
