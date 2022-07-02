97f7e967e508dd4d9634e602b0bd100e1f6bb576
vladislavikys <49569336+vladislavikys@users.noreply.github.com>
2022-07-02 13:34:26 +0300

    plus progressView

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -1,39 +1,40 @@
-//
-//  ViewController.swift
-//  EggTimer
-//
-//  Created by Angela Yu on 08/07/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
     
     
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var progressView: UIProgressView!
+    
     
     let eggTimes = [ "Soft": 3 , "Medium": 4 ,"Hard": 7]
     var secondRemaining = 0
     var timer = Timer()
+    var totalTime = 0
+    var secondsPassed = 0
    
     @IBAction func hardnessSelected(_ sender: UIButton) {
         let  hardness = sender.currentTitle!
         timer.invalidate()
         
-        //получаем силу варки яйца
-        
-        secondRemaining  =  eggTimes[hardness]!
+        progressView.setProgress(0, animated: true)
+        secondsPassed = 0
+        titleLabel?.text = hardness
+        totalTime  =  eggTimes[hardness]!
         // на основе этого опбращаемся по ключу за время  готовки
         
-       timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
     }
     
     @objc func updateCounter() {
        //example functionality
-       if secondRemaining > 0  {
-           print("\(secondRemaining)")
-           secondRemaining -= 1
+       if secondsPassed < totalTime  {
+
+           secondsPassed += 1
+           progressView.progress = Float(secondsPassed) / Float(totalTime)
+           
+           
+           
        }else if secondRemaining == 0 {
            timer.invalidate()
            titleLabel.text = "DONE!"
